278f0e06f53e8a08b490109094ab9bca9b0b5ccf
Joren De Groof <info@jorendegroof.be>
2010-02-16 11:04:00 +0100

    updated myspace api keys

--- app/vendors/myspace/config/config.MySpace.php
+++ app/vendors/myspace/config/config.MySpace.php
@@ -1,10 +1,10 @@
 <?php
 
 	//please register at http://developer.myspace.com for a CONSUMERK_KEY
-	define('CONSUMER_KEY', '59f0f7ec119649698d1c5710afc29824');
+	define('CONSUMER_KEY', 'c87b1a3237324b5eaed23749e9ebff37');
 //	define('CONSUMER_KEY', 'http://www.myspace.com/499532349');
 	//please register at http://developer.myspace.com for a CONSUMER_SECRET
-	define('CONSUMER_SECRET', '2cb0a56532434ae7b34b9f95253e3082ccd08b5079354d208378a13cc1a44c5f');
+	define('CONSUMER_SECRET', '46aed6a612744b80aab0c19cbb488140ee2bea3a7add45f982ec3765bc48c76e');
 //	define('CONSUMER_SECRET', 'ab913d3ca03b4da2ba17e1815bf960b6156241e1a07f4a66a5a1ed665a02c87e');
 
 	/**
